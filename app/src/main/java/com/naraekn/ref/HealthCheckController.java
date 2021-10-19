fde30122d0ce1d1635439b3fb8275961dc863647
nankim <nan.kim@kurlycorp.com>
2021-10-19 22:40:11 +0900

    Add health check controller

---
+++ app/src/main/java/com/naraekn/ref/HealthCheckController.java
@@ -0,0 +1,19 @@
+package com.naraekn.ref;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@RestController
+@RequestMapping("/health-check")
+public class HealthCheckController {
+    @GetMapping
+    String healthCheck() {
+        Date dateTime = new Date();
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss z");
+        return formatter.format(dateTime);
+    }
+}
